3eb98c451391a1f7f744800b479575b7c73387e3
Hhenry88 <henryhan88@outlook.com>
2017-08-25 16:49:10 +0800

    Methods

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 //create a Vue instance
 new Vue({
   //which element will this instance be shown on
-  el: '#vue-app',
+  el: `#vue-app`,
   data: {
-    name: 'Henryyy'
+    name: 'Henryyy',
+    job:`developerrr`,
+  },
+  methods: {
+    //if using arrow functions, will not get this. this will not be the method.
+    welcome: function(hi){
+      return `${hi} welcome to my page. I'm ${this.name} and I'm a ${this.job}`;
+    }
   }
 });
