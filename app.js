655af94e0a3e3a9b0f080c8dc1d1faa392361685
Hhenry88 <henryhan88@outlook.com>
2017-08-26 10:47:13 +0800

    Events and using offsetX and Y to track mouse movement over a canvas

--- app.js
+++ app.js
@@ -5,13 +5,27 @@ new Vue({
   data: {
     name: 'Henryyy',
     job:`developerrr`,
+    age: 25,
     website: `http://github.com/HHenry88`,
-    websiteTag: `<a href="http://github.com/HHenry88">My website tag</a>`
+    websiteTag: `<a href="http://github.com/HHenry88">My website tag</a>`,
+    x: 0,
+    y: 0
   },
   methods: {
     //if using arrow functions, will not get this. this will not be the method.
     welcome: function(hi){
       return `${hi} welcome to my page. I'm ${this.name} and I'm a ${this.job}`;
+    },
+    add: function(){
+      this.age++;
+    },
+    subtract: function(){
+      this.age--;
+    },
+    updateXY: function(event){
+      console.log(event);
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
